a95f696e63d71013c76f413f2791b479f96f93d6
Roman Paradeev <roman.paradeev@klarna.com>
2020-04-02 17:02:23 +0200

    mix

--- desktop.blocks/product-list/product-list.bemhtml.js
+++ desktop.blocks/product-list/product-list.bemhtml.js
@@ -1,13 +1,13 @@
 block('product-list')(
     {
+        tag: 'ul',
         content: (node, ctx) => [
             ...ctx.products.map(product => ({
-                elem: 'item',
-                content: {
-                    block: 'product',
-                    title: product.title,
-                    tagline: product.tagline
-                }
+                tag: 'li',
+                mix: { elem: 'item', block: 'product-list' },
+                block: 'product',
+                title: product.title,
+                tagline: product.tagline
             }))
         ]
     },
